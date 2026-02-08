59e922cc10974c05d5b0cd95a74927b9c39f23d7
arisa <98852332+Unonkgw@users.noreply.github.com>
2026-02-08 23:28:14 +0800

    feat: email and cloudinary config updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -171,11 +171,12 @@ CSRF_TRUSTED_ORIGINS = [
 # 2. PRODUCTION MODE (Uncomment when ready)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_TIMEOUT = 10
 
 
 # --- CLOUDINARY CONFIGURATION ---
